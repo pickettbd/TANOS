78458c7e8e5f71e4b82b94df38d31205526252d1
Brandon Pickett <pickettbd@gmail.com>
2020-06-30 11:15:48 -0600

    added pkgutil to install_requires

--- setup.py
+++ setup.py
@@ -162,7 +162,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=["sys", "re", "argparse", "pathlib"],  # Optional
+    install_requires=["sys", "re", "pkgutil", "argparse", "pathlib"],  # Optional
 
 	# setup_requires
 	setup_requires=["pathlib"],  # Optional
